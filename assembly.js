43848940ae33d5b354afa2d41573e0c71b684b36
Marco Costalba <mcostalba@users.noreply.github.com>
2015-12-27 12:17:27 +0100

    Fix bug on first operand
    To shift 4 bits we need to multiply by 16, not 4.

--- assembly.js
+++ assembly.js
@@ -5,11 +5,21 @@ var ISA = {};
 // Here is defined our ISA set
 //
 // ***************************************************************************
-ISA['NOP'] = { opcode:'00', parse_op:parse_op_0 };
-ISA['ADC'] = { opcode:'02', parse_op:parse_op_3 };
+ISA['NOP']  = { opcode:'00', parse_op:parse_op_0 };
+ISA['ADC']  = { opcode:'02', parse_op:parse_op_3 };
+ISA['ADCI'] = { opcode:'04', parse_op:parse_op_2_nnnn };
+ISA['ADD']  = { opcode:'06', parse_op:parse_op_3 };
+ISA['ADDI'] = { opcode:'08', parse_op:parse_op_2_nnnn };
 
 
 
+// Parse a 16 bit constant number
+function parse_h16(reg) {
+  
+  id = Number(reg);
+  if (!isNaN(id) && id >= 0 && id <= 65535)
+      return id;
+}
 
 
 // Parse a register menmonic and return the corresponding 4 bit number: R3 -> 03
@@ -42,9 +52,25 @@ function parse_op_3(op) {
     rf = parse_reg(op[1].trim());
     rs = parse_reg(op[2].trim());
     if (rd != undefined && rf != undefined && rs != undefined)
-      return { type:'r3', data:[rd, rf, rs] };
+      return { type:'3', data:[rd, rf, rs] };
   }
 }
+
+
+// Parse instructions with 2 registers and one constant
+function parse_op_2_nnnn(op) {
+  
+  op = op.split(',');
+  if (op.length == 3)
+  {
+    rd = parse_reg(op[0].trim());
+    rf = parse_reg(op[1].trim());
+    h16 = parse_h16(op[2].trim());
+    if (rd != undefined && rf != undefined && h16 != undefined)
+      return { type:'3', data:[rd, rf, h16] };
+  }
+}
+
 
 
 // Emit the assembly code for the given instruction
@@ -52,9 +78,9 @@ function emit(code, operands) {
   
   ins = ISA[code].opcode;
   
-  if (operands.type == 'r3')
+  if (operands.type == '3')
   {
-    msb = '00' + Number(operands.data[0] * 4 + operands.data[1]).toString(16).toUpperCase();
+    msb = '00' + Number(operands.data[0] * 16 + operands.data[1]).toString(16).toUpperCase();
     lsb = '0000' + Number(operands.data[2]).toString(16).toUpperCase();
     ins = ins + msb.substring(msb.length - 2) + lsb.substring(lsb.length - 4);
   }
