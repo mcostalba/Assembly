8159b9629de1370f89fd00dcd23cb004c2c4b929
Marco Costalba <mcostalba@users.noreply.github.com>
2016-01-26 08:21:02 +0100

    Add label support
    It is now possible to use labels for absolute and relative branches

--- assembly.js
+++ assembly.js
@@ -48,46 +48,46 @@ ISA['INV']  = { opcode:'4D', format:[pr, p0, pr] };
 ISA['LD']   = { opcode:'4F', format:[pr, p0, pr] };
 ISA['LDI']  = { opcode:'51', format:[pr, p0, pn] };
 
-ISA['LDA']  = { opcode:'53', format:[pr, p0, pni] };
-ISA['LDAC'] = { opcode:'55', format:[pr, p0, pni] };
-ISA['LDAD'] = { opcode:'57', format:[pr, p0, pni] };
-ISA['LDAE'] = { opcode:'59', format:[pr, p0, pni] };
-ISA['LDAF'] = { opcode:'5B', format:[pr, p0, pni] };
+ISA['LDA']  = { opcode:'53', format:[pr, p0, pai] };
+ISA['LDAC'] = { opcode:'55', format:[pr, p0, pai] };
+ISA['LDAD'] = { opcode:'57', format:[pr, p0, pai] };
+ISA['LDAE'] = { opcode:'59', format:[pr, p0, pai] };
+ISA['LDAF'] = { opcode:'5B', format:[pr, p0, pai] };
 ISA['LDR']  = { opcode:'5D', format:[pr, p0, pri] };
 ISA['LDRC'] = { opcode:'5F', format:[pr, p0, pri] };
 ISA['LDRD'] = { opcode:'61', format:[pr, p0, pri] };
 ISA['LDRE'] = { opcode:'63', format:[pr, p0, pri] };
 ISA['LDRF'] = { opcode:'65', format:[pr, p0, pri] };
-ISA['STA']  = { opcode:'67', format:[p0, pr, pni] };
-ISA['STAC'] = { opcode:'69', format:[p0, pr, pni] };
-ISA['STAD'] = { opcode:'6B', format:[p0, pr, pni] };
-ISA['STAE'] = { opcode:'6D', format:[p0, pr, pni] };
-ISA['STAF'] = { opcode:'6F', format:[p0, pr, pni] };
+ISA['STA']  = { opcode:'67', format:[p0, pr, pai] };
+ISA['STAC'] = { opcode:'69', format:[p0, pr, pai] };
+ISA['STAD'] = { opcode:'6B', format:[p0, pr, pai] };
+ISA['STAE'] = { opcode:'6D', format:[p0, pr, pai] };
+ISA['STAF'] = { opcode:'6F', format:[p0, pr, pai] };
 ISA['STR']  = { opcode:'71', format:[p0, pr, pri] };
 ISA['STRC'] = { opcode:'73', format:[p0, pr, pri] };
 ISA['STRD'] = { opcode:'75', format:[p0, pr, pri] };
 ISA['STRE'] = { opcode:'77', format:[p0, pr, pri] };
 ISA['STRF'] = { opcode:'79', format:[p0, pr, pri] };
 
-ISA['JP']   = { opcode:'7B', format:[p0, p0, pn] };
-ISA['JPN']  = { opcode:'7D', format:[p0, p0, pn] };
-ISA['JPP']  = { opcode:'7F', format:[p0, p0, pn] };
-ISA['JPZ']  = { opcode:'81', format:[p0, p0, pn] };
-ISA['JPNZ'] = { opcode:'83', format:[p0, p0, pn] };
-ISA['JPPO'] = { opcode:'85', format:[p0, p0, pn] };
-ISA['JPNO'] = { opcode:'87', format:[p0, p0, pn] };
-ISA['JPC']  = { opcode:'89', format:[p0, p0, pn] };
-ISA['JPNC'] = { opcode:'8B', format:[p0, p0, pn] };
+ISA['JP']   = { opcode:'7B', format:[p0, p0, pa] };
+ISA['JPN']  = { opcode:'7D', format:[p0, p0, pa] };
+ISA['JPP']  = { opcode:'7F', format:[p0, p0, pa] };
+ISA['JPZ']  = { opcode:'81', format:[p0, p0, pa] };
+ISA['JPNZ'] = { opcode:'83', format:[p0, p0, pa] };
+ISA['JPPO'] = { opcode:'85', format:[p0, p0, pa] };
+ISA['JPNO'] = { opcode:'87', format:[p0, p0, pa] };
+ISA['JPC']  = { opcode:'89', format:[p0, p0, pa] };
+ISA['JPNC'] = { opcode:'8B', format:[p0, p0, pa] };
 
-ISA['JR']   = { opcode:'8D', format:[p0, p0, pn] };
-ISA['JRN']  = { opcode:'8F', format:[p0, p0, pn] };
-ISA['JRP']  = { opcode:'91', format:[p0, p0, pn] };
-ISA['JRZ']  = { opcode:'93', format:[p0, p0, pn] };
-ISA['JRNZ'] = { opcode:'95', format:[p0, p0, pn] };
-ISA['JRPO'] = { opcode:'97', format:[p0, p0, pn] };
-ISA['JRNO'] = { opcode:'99', format:[p0, p0, pn] };
-ISA['JRC']  = { opcode:'9B', format:[p0, p0, pn] };
-ISA['JRNC'] = { opcode:'9D', format:[p0, p0, pn] };
+ISA['JR']   = { opcode:'8D', format:[p0, p0, po] };
+ISA['JRN']  = { opcode:'8F', format:[p0, p0, po] };
+ISA['JRP']  = { opcode:'91', format:[p0, p0, po] };
+ISA['JRZ']  = { opcode:'93', format:[p0, p0, po] };
+ISA['JRNZ'] = { opcode:'95', format:[p0, p0, po] };
+ISA['JRPO'] = { opcode:'97', format:[p0, p0, po] };
+ISA['JRNO'] = { opcode:'99', format:[p0, p0, po] };
+ISA['JRC']  = { opcode:'9B', format:[p0, p0, po] };
+ISA['JRNC'] = { opcode:'9D', format:[p0, p0, po] };
 
 ISA['LDSP'] = { opcode:'9F', format:[p0, p0, p0] };
 ISA['POP']  = { opcode:'A1', format:[pr, p0, p0] };
@@ -95,16 +95,16 @@ ISA['PUSH'] = { opcode:'A4', format:[p0, pr, p0] };
 ISA['POPF'] = { opcode:'A7', format:[p0, p0, p0] };
 ISA['PUSHF']= { opcode:'AA', format:[p0, p0, p0] };
 
-ISA['CALL'] = { opcode:'AD', format:[p0, p0, pn] };
+ISA['CALL'] = { opcode:'AD', format:[p0, p0, pa] };
 ISA['RET']  = { opcode:'B1', format:[p0, p0, p0] };
 ISA['JPM']  = { opcode:'B4', format:[p0, p0, pri] };
 ISA['JPR']  = { opcode:'B6', format:[p0, p0, pr] };
 
-ISA['OTA']  = { opcode:'B8', format:[pp, p0, pni] };
-ISA['OTAC'] = { opcode:'BA', format:[pp, p0, pni] };
-ISA['OTAD'] = { opcode:'BC', format:[pp, p0, pni] };
-ISA['OTAE'] = { opcode:'BE', format:[pp, p0, pni] };
-ISA['OTAF'] = { opcode:'C0', format:[pp, p0, pni] };
+ISA['OTA']  = { opcode:'B8', format:[pp, p0, pai] };
+ISA['OTAC'] = { opcode:'BA', format:[pp, p0, pai] };
+ISA['OTAD'] = { opcode:'BC', format:[pp, p0, pai] };
+ISA['OTAE'] = { opcode:'BE', format:[pp, p0, pai] };
+ISA['OTAF'] = { opcode:'C0', format:[pp, p0, pai] };
 ISA['OUT']  = { opcode:'C2', format:[pp, pr, p0] };
 ISA['OTI']  = { opcode:'C4', format:[pp, p0, pn] };
 ISA['OTR']  = { opcode:'C6', format:[pp, p0, pri] };
@@ -113,11 +113,11 @@ ISA['OTRD'] = { opcode:'CA', format:[pp, p0, pri] };
 ISA['OTRE'] = { opcode:'CC', format:[pp, p0, pri] };
 ISA['OTRF'] = { opcode:'CE', format:[pp, p0, pri] };
 
-ISA['INA']  = { opcode:'D0', format:[p0, pp, pni] };
-ISA['INAC'] = { opcode:'D2', format:[p0, pp, pni] };
-ISA['INAD'] = { opcode:'D4', format:[p0, pp, pni] };
-ISA['INAE'] = { opcode:'D6', format:[p0, pp, pni] };
-ISA['INAF'] = { opcode:'D8', format:[p0, pp, pni] };
+ISA['INA']  = { opcode:'D0', format:[p0, pp, pai] };
+ISA['INAC'] = { opcode:'D2', format:[p0, pp, pai] };
+ISA['INAD'] = { opcode:'D4', format:[p0, pp, pai] };
+ISA['INAE'] = { opcode:'D6', format:[p0, pp, pai] };
+ISA['INAF'] = { opcode:'D8', format:[p0, pp, pai] };
 ISA['IN']   = { opcode:'DA', format:[pn, pp, p0] };
 ISA['INR']  = { opcode:'DC', format:[p0, pp, pri] };
 ISA['INRC'] = { opcode:'DE', format:[p0, pp, pri] };
@@ -135,6 +135,20 @@ ISA['SPRE'] = { opcode:'F2', format:[p0, pn, pn] };
 ISA['SPRF'] = { opcode:'F4', format:[p0, pn, pn] };
 
 
+// A comma separated string like address0,label0,address1,label1,...
+var LABELS = [];
+var MYADDR;
+
+// Look-up a given label and return the address if found. Comparison
+// is case-insensitive.
+function lookup(label) {
+
+  for (var i = 1; i < LABELS.length; i += 2) {
+    if (LABELS[i].toUpperCase() == label.toUpperCase())
+       return parseInt(LABELS[i-1], 16);
+  }
+}
+
 // Ignore argument
 function p0(reg) { return 0; }
 
@@ -143,10 +157,9 @@ function p0(reg) { return 0; }
 function pn(reg) {
 
   var id = parseInt(reg.trim());
-  if (!isNaN(id))
-  {
+  if (!isNaN(id)) {
     if (id < 0)
-      id = 0xFFFF + id; // Two's complement
+      id = 0xFFFF + id + 1; // Two's complement
     
     if (id >= 0 && id <= 0xFFFF)
       return id;
@@ -185,22 +198,51 @@ function pp(reg) {
     return parseInt(reg, 16);
 }
 
-// Our main function, parse an instruction and returns the corresponding code. This
-// function is the only one called from outside this module.
+// Parse an absolute address out of a number or label
+function pa(reg) {
+  
+  reg = reg.trim()
+  var addr = pn(reg)
+  if (!isNaN(addr))
+    return addr;
+  
+  return lookup(reg);
+}
+
+// Parse an indirect absolute address
+function pai(reg) {
+
+  reg = reg.trim()
+  if (reg.length > 2 && reg[0] == '(' && reg[reg.length - 1] == ')')
+    return pa(reg.substring(1, reg.length - 1));
+}
+
+// Parse a relative offset out of a number or label
+function po(reg) {
+  
+  reg = reg.trim()
+  var addr = pa(reg)
+  if (!isNaN(addr))
+    return pn(String(addr - MYADDR))
+}
+
+// Our main function, parse an instruction and returns the corresponding code.
+// This function is the only one called from outside this module.
 function parse2(m, d, a, l) {
 
   var Shifts = [4, 4, 16];
 
   var code = String(m).trim().toUpperCase();
-  var op = String(d).toUpperCase().split(',');
-  //var addr = String(a).trim().toUpperCase();
-  //var labels = String(Array(l)[0]).split(',');
-
+  var op = String(d).trim().toUpperCase().split(',');
+  var addr = String(a).trim().toUpperCase();
+  
   if (!(code in ISA))
     return '-99';
+  
+  LABELS = l.split(',');
+  MYADDR = parseInt(addr, 16);
 
   var ins = 0, idx = 0;
-
   ISA[code].format.forEach(function(fun, pos) {
 
     ins = (ins << Shifts[pos]) + fun(op[idx]);
@@ -209,5 +251,8 @@ function parse2(m, d, a, l) {
        idx = idx + 1;
   })
 
+  if (idx < op.length && op[0] != '') // Split() returns non-empty array
+     return 'too many arguments';
+
   return ISA[code].opcode + ('000000' + ins.toString(16).toUpperCase()).substr(-6);
-};
+}
